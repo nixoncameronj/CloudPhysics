f92fc07d5ca40888cba126b9f213a66e028ef878
Cameron Nixon <nixon.cameronj@gmail.com>
2018-10-25 11:26:10 -0500

    fixed rows/columns being read improperly

--- readsoundings.py
+++ readsoundings.py
@@ -1,12 +1,18 @@
 import numpy as np
 
-def parse_SPC(filename, skip_rows=6):
+def parse_SPC(filename, skip_rows=5):
 	dtype = [('p', float),		# pressure, mb
 	         ('z', float),		# altitude, m
 		 ('T', float),		# temperature, C
 		 ('Td', float),		# dewpoint, C
+                 ('RH', float),		# relative humidity (%)
+                 ('w', float),          # mixing ratio (g/kg)
 		 ('wind_dir', float),	# wind direction, degrees
-		 ('wind_spd', float)	# wind speed, knots
+		 ('wind_spd', float),	# wind speed, knots
+                 ('theta', float),      # theta, K
+                 ('theta-e', float),    # theta-e, K
+                 ('theta-v', float),    # theta-v, K
+
 		 ]
-	data = np.genfromtxt(filename, dtype=dtype, skip_header=1, delimiter=',')
+	data = np.genfromtxt(filename, dtype=dtype, skip_header=skip_rows, skip_footer=80)
 	return data
