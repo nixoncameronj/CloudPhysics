3c4e35cf4bf0086316ac22a6208a19b1c68cfe81
Cameron Nixon <nixon.cameronj@gmail.com>
2018-10-25 13:30:16 -0500

    can now plot theta-ep

--- skewt.py
+++ skewt.py
@@ -68,14 +68,22 @@ def from_thermo(T_C, p):
 def theta_e(T, p, p_0):
     '''Calculates theta_e from T, p, p_0
     '''
-    R_d = 287
+    R_d = 287.1
     c_l = 4218
-    c_p = 1882
+    c_p = 1005
     w_s = Bolton.sat_mixing_ratio(p, T)
     w_t = w_s
-    C_wd = c_pd + (w_t*c_l)
+    C_wd = c_pd + (w_s*c_l)
     L_v = (3.139*(10**6)) - (c_l - c_p)*T
     theta_e = T*((p/p_0)**(R_d/c_wd))*np.exp((L_v*w_s)/(c_wd*T))
+    return theta_e
+    
+def theta_e_lines(T, p, p_0=1000.0):
+    '''calculates theta-e lines
+    '''
+    w = sat_mixing_ratio(p, T)
+    theta_e_lines = theta_e(T, p, w, p_0)
+    return theta_e_lines
 
 # values along the bottom and left edges
 p_bottom = 1050.0
@@ -91,12 +99,13 @@ y_max = y_from_p(p_top)
 
 # define constant levels
 p_levels = np.arange(1000, 150-50, -50)
-T_C_levels = np.arange(-80, 40+10, 10)
+T_C_levels = np.arange(-120, 40+10, 10)
 T_levels = T_C_levels + C_to_K
 P_levels = np.arange(1000, 150-50, -50)
-theta_levels = (np.arange(-40, 100_10, 10)) + C_to_K
+theta_levels = (np.arange(-40, 100+10, 10)) + C_to_K
+theta_e_levels = (np.arange(-40, 100+10, 10)) + C_to_K
 theta_ep_levels = theta_levels.copy()
-mixing_ratios = np.asarray([.4,1,2,3,5,8,12,16,20])
+mixing_ratios = np.asarray([.0004,.001,.002,.003,.005,.008,.012,.016,.020])
 
 
 # In[19]:
@@ -107,7 +116,9 @@ y_p_levels = y_from_p(p_levels)
 y_all_p = y_from_p(p_all)
 x_T_levels = [x_from_Tp(Ti, p_all) for Ti in T_levels]
 x_thetas = [x_from_Tp(Bolton.theta_dry(theta_i, p_all),p_all) for theta_i in theta_levels]
-x_mixing_ratios = [x_from_Tp(Bolton.mixing_ratio_line(p_all,mixing_ratios_i),p_all) for mixing_ratios_i in mixing_ratios]
+x_mixing_ratios = [x_from_Tp(Bolton.mixing_ratio_line(p_all,mixing_ratio_i)+C_to_K,p_all) for mixing_ratio_i in mixing_ratios]
+x_thetaes = [x_from_Tp(Bolton.theta_dry(thetae_i, p_all),p_all) for thetae_i in theta_e_levels]
+
 
 mesh_T, mesh_p = np.meshgrid(np.arange(-60.0, T_levels.max()-C_to_K+0.1, 0.1), p_all)
 theta_ep_mesh = Bolton.theta_ep_field(mesh_T, mesh_p)
@@ -121,12 +132,12 @@ y_snd_p = y_from_p(snd_p)
 snd_T = sounding_data['T']
 # all temperature values, deg. C, should be in this range.
 good_T = (snd_T > -100.0) & (snd_T < 60.0)
-x_snd_T = x_from_Tp(snd_T, snd_p)
+x_snd_T = x_from_Tp(snd_T, snd_p)+C_to_K
 y_snd_T = y_from_p(snd_p)
 
 snd_Td = sounding_data['Td']
 good_Td = (snd_Td > -100.0) & (snd_Td < 60.0)
-x_snd_Td = x_from_Tp(snd_Td, snd_p)
+x_snd_Td = x_from_Tp(snd_Td, snd_p)+C_to_K
 y_snd_Td = y_from_p(snd_p)
 
 #
@@ -135,7 +146,7 @@ y_snd_Td = y_from_p(snd_p)
 # In[20]:
 
 
-
+%matplotlib inline
 skew_grid_helper = GridHelperCurveLinear((from_thermo, to_thermo))
 fig = plt.figure()
 ax = Subplot(fig,1,1,1,grid_helper = skew_grid_helper)
@@ -150,9 +161,10 @@ for yi in y_p_levels:
     
 for x_T in x_T_levels:
     ax.plot(x_T, y_all_p, color=(1.0, 0.5, 0.5))
-    
+
+#wrong?
 for x_theta in x_thetas:
-    ax.plot(x_theta, y_all_p, color=(1.0, 0.7, 0.7))
+    ax.plot(x_theta-C_to_K, y_all_p, color=(1.0, 0.7, 0.7))
     
 for x_mixing_ratio in x_mixing_ratios:
     good = p_all >= 600 # restrict mixing ratio lines to below 600 mb
@@ -169,5 +181,6 @@ ax.plot(x_snd_T, y_snd_p, linewidth=2, color='r')
 
 ax.axis((x_min, x_max, y_min, y_max))
 
-plt.show()
+#ax.set_xlim(-40,0)
 
+plt.show()
