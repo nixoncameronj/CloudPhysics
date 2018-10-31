6af6b53a4240d0bccea18e222b1199179903d178
Cameron <nixon.cameronj@gmail.com>
2018-10-31 17:08:15 -0500

    theta-dry works!

--- skewt.py
+++ skewt.py
@@ -14,7 +14,7 @@ from mpl_toolkits.axisartist.grid_helper_curvelinear import GridHelperCurveLinea
 
 from readsoundings import parse_SPC
 
-sounding = '/home/cameron/Documents/Python/git_repos/CloudPhysics/april9sounding'
+sounding = '/home/cjnix/cjnix/Documents/Python/git_repos/CloudPhysics/april9sounding'
 sounding_data = parse_SPC(sounding)
 
 
@@ -59,31 +59,31 @@ def to_thermo(x, y):
     return T_C, p
 
 def from_thermo(T_C, p):
-    '''Transform T_C (in degrees Celsius) 
+    '''Transform T_C (in degrees Celsius)
        and p (in mb) to (x,y)'''
     y = y_from_p(p)
     x = x_from_Tp(T_C+C_to_K, p)
     return x, y
 
-def theta_e(T, p, p_0):
-    '''Calculates theta_e from T, p, p_0
-    '''
-    R_d = 287.1
-    c_l = 4218
-    c_p = 1005
-    w_s = Bolton.sat_mixing_ratio(p, T)
-    w_t = w_s
-    C_wd = c_pd + (w_s*c_l)
-    L_v = (3.139*(10**6)) - (c_l - c_p)*T
-    theta_e = T*((p/p_0)**(R_d/c_wd))*np.exp((L_v*w_s)/(c_wd*T))
-    return theta_e
-    
-def theta_e_lines(T, p, p_0=1000.0):
-    '''calculates theta-e lines
-    '''
-    w = sat_mixing_ratio(p, T)
-    theta_e_lines = theta_e(T, p, w, p_0)
-    return theta_e_lines
+# def theta_e(T, p, p_0):
+#     '''Calculates theta_e from T, p, p_0
+#     '''
+#     R_d = 287.1
+#     c_l = 4218
+#     c_p = 1005
+#     w_s = Bolton.sat_mixing_ratio(p, T)
+#     w_t = w_s
+#     C_wd = c_pd + (w_s*c_l)
+#     L_v = (3.139*(10**6)) - (c_l - c_p)*T
+#     theta_e = T*((p/p_0)**(R_d/c_wd))*np.exp((L_v*w_s)/(c_wd*T))
+#     return theta_e
+
+# def theta_e_lines(T, p, p_0=1000.0):
+#     '''calculates theta-e lines
+#     '''
+#     w = sat_mixing_ratio(p, T)
+#     theta_e_lines = theta_e(T, p, w, p_0)
+#     return theta_e_lines
 
 # values along the bottom and left edges
 p_bottom = 1050.0
@@ -119,10 +119,9 @@ x_thetas = [x_from_Tp(Bolton.theta_dry(theta_i, p_all),p_all) for theta_i in the
 x_mixing_ratios = [x_from_Tp(Bolton.mixing_ratio_line(p_all,mixing_ratio_i)+C_to_K,p_all) for mixing_ratio_i in mixing_ratios]
 x_thetaes = [x_from_Tp(Bolton.theta_dry(thetae_i, p_all),p_all) for thetae_i in theta_e_levels]
 
-
 mesh_T, mesh_p = np.meshgrid(np.arange(-60.0, T_levels.max()-C_to_K+0.1, 0.1), p_all)
 theta_ep_mesh = Bolton.theta_ep_field(mesh_T, mesh_p)
-
+theta_e_mesh = Bolton.theta_e_field(mesh_T, mesh_p)
 
 # sounding data
 snd_p = sounding_data['p']
@@ -158,22 +157,28 @@ fig.add_subplot(ax)
 
 for yi in y_p_levels:
     ax.plot((x_min, x_max), (yi,yi), color=(1.0, 0.8, 0.8))
-    
+
 for x_T in x_T_levels:
     ax.plot(x_T, y_all_p, color=(1.0, 0.5, 0.5))
 
-#wrong?
+#doesn't work!
 for x_theta in x_thetas:
-    ax.plot(x_theta-C_to_K, y_all_p, color=(1.0, 0.7, 0.7))
-    
+    ax.plot(x_theta, y_all_p, color=(1.0, 0.7, 0.7))
+
 for x_mixing_ratio in x_mixing_ratios:
     good = p_all >= 600 # restrict mixing ratio lines to below 600 mb
     ax.plot(x_mixing_ratio[good], y_all_p[good], color=(0.8, .8, 0.6))
-    
+
 n_moist = len(theta_ep_levels)
 moist_colors = ((0.6,0.9,0.7),)*n_moist
 ax.contour(x_from_Tp(mesh_T+C_to_K, mesh_p), y_from_p(mesh_p),
     theta_ep_mesh, theta_ep_levels, colors=moist_colors)
+
+#doesn't work!
+theta_e_lines = len(theta_e_levels)
+moist_colors = ((0.6,0.9,0.7),)*theta_e_lines
+ax.contour(x_from_Tp(mesh_T+C_to_K, mesh_p), y_from_p(mesh_p),
+    theta_e_mesh, theta_e_levels, colors='lightblue')
 
 ax.plot(x_snd_Td, y_snd_p, linewidth=2, color='g')
 ax.plot(x_snd_T, y_snd_p, linewidth=2, color='r')
